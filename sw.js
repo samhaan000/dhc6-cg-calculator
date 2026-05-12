8f7753540999c8a62bc842e58099d80a1b408493
samhaan000 <samhaan000@gmail.com>
2026-05-12 21:53:35 +0500

    Bump offline cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dhc6-cg-calculator-v1';
+const CACHE_NAME = 'dhc6-cg-calculator-v2';
 const FILES_TO_CACHE = [
   './',
   './index.html',
@@ -8,21 +8,15 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(FILES_TO_CACHE); }));
 });
 
 self.addEventListener('activate', function(event) {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(key) { return key !== CACHE_NAME; }).map(function(key) { return caches.delete(key); }));
+  }).then(function() { return self.clients.claim(); }));
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request).catch(function() { return caches.match(event.request); }));
 });
